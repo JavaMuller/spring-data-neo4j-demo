ec50f69f4310213929ea5412c92237f58fd786b4
Volkan Yazıcı <volkan.yazici@gmail.com>
2015-01-14 23:51:47 +0100

    Disable Spring Boot banner.

--- src/main/java/com/vlkan/springdataneo4jdemo/Application.java
+++ src/main/java/com/vlkan/springdataneo4jdemo/Application.java
@@ -38,7 +38,9 @@ public class Application implements CommandLineRunner {
     }
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication app = new SpringApplication(Application.class);
+        app.setShowBanner(false);
+        app.run(args);
     }
 
 }
